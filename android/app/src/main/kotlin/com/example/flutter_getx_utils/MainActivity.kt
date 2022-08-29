045b61ddc80e3f50734080b4486667ba4cbba9d2
alish <alishojib12@gmail.com>
2022-08-29 17:17:55 +0600

    Gex snackbar util all perms in main.dart

---
+++ android/app/src/main/kotlin/com/example/flutter_getx_utils/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_getx_utils
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      title: 'GetX',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Snackbar'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Get.snackbar(
+                    "Snackbar Title",
+                    "This will be snackbar message",
+                    titleText: Text("Another Title"),
+                    messageText: Text("Another Message"),
+                    snackPosition: SnackPosition.BOTTOM,
+                    backgroundColor: Colors.white70,
+                    borderRadius: 30.0,
+                    margin: EdgeInsets.all(30.0),
+                    maxWidth: double.infinity,
+                    backgroundGradient: const LinearGradient(
+                        colors: [Colors.red, Colors.green]),
+                    borderColor: Colors.yellow,
+                    borderWidth: 4,
+                    boxShadows: [
+                      const BoxShadow(
+                          color: Colors.black,
+                          offset: Offset(30, 50),
+                          spreadRadius: 20.0,
+                          blurRadius: 8.0),
+                    ],
+                    isDismissible: true,
+                    dismissDirection: DismissDirection.horizontal,
+                    forwardAnimationCurve: Curves.bounceInOut,
+                    duration: Duration(milliseconds: 8000),
+                    icon: Icon(
+                      Icons.send,
+                      color: Colors.white,
+                    ),
+                    shouldIconPulse: true,
+                    leftBarIndicatorColor: Colors.red,
+                    mainButton: TextButton(
+                      onPressed: () {},
+                      child: Text("Retry"),
+                    ),
+                    onTap: (val) {
+                      print("Snackbar Clicked");
+                    },
+                    overlayBlur: 5.0,
+                    overlayColor: Colors.black,
+                    padding: EdgeInsets.all(50),
+                    showProgressIndicator: true,
+                    progressIndicatorBackgroundColor: Colors.deepOrange,
+                    progressIndicatorValueColor:
+                        const AlwaysStoppedAnimation<Color>(Colors.white),
+                    reverseAnimationCurve: Curves.bounceInOut,
+                    snackbarStatus: (val) {
+                      print(val);
+                    },
+                    // This overrides the entire UI elements:
+                    // userInputForm: Form(
+                    //   child: Row(
+                    //     children: [
+                    //       Expanded(child: TextField(
+                    //       ))
+                    //     ],
+                    //   ),
+                    // )
+                  );
+                },
+                child: const Text('Show Snackbar')),
+          ],
+        ),
+      ),
+    );
+  }
+}
