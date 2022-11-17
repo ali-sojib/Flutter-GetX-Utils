796f60534e547ae4e3bb543a25919395c3362427
Ali <alishojib12@gmail.com>
2022-11-17 18:53:09 +0600

    dependency_injection in getx without run in mobile implimented

--- lib/getx_dependency_injection/controller_dependency_injection.dart
+++ lib/getx_dependency_injection/controller_dependency_injection.dart
@@ -4,7 +4,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 class ControllerDependencyInjection extends GetxController{
   void incrementCounter() async{
     SharedPreferences preferences = await SharedPreferences.getInstance();
-    int counter
-
+    int counter = (preferences.getInt('counter')??0)+1;
+    print('Pressed $counter times');
+    await preferences.setInt('counter', counter);
   }
 }
--- lib/getx_dependency_injection/main_dependency_injeciton.dart
+++ lib/getx_dependency_injection/main_dependency_injeciton.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import 'controller_dependency_injection.dart';
+
 
 class GetXDependencyInjection extends StatelessWidget {
   const GetXDependencyInjection({Key? key}) : super(key: key);
@@ -9,41 +14,40 @@ class GetXDependencyInjection extends StatelessWidget {
     // tag will be used to find the instance with tag name
     // Controller disposed when it is not used but if permanent is
     //true the instance will be alive throughout the app
-    MyController myController = Get.put(MyController(), tag: "instance1", permanent: true);
+    // ControllerDependencyInjection myController = Get.put(ControllerDependencyInjection(), tag: "instance1", permanent: true);
 
     // Instance will be created when it is used
     //It is similar to "permanent", the difference is that the instance
     // is discarded when is not being used,
     // but when it's use is needed again, Get will recreate the instance
-    //Get.lazyPut(() => MyController(),tag:"instance",fenix: false);
+    // Get.lazyPut(() => ControllerDependencyInjection(),tag:"instance",fenix: false);
 
-    // Get.putAsync<MyController>(() async => await MyController());
+    Get.putAsync<ControllerDependencyInjection>(() async => await ControllerDependencyInjection());
 
 // Here permanent will be true by default and isSingleton is false
 //    Get.Create<SomeClass>(() => SomeClass());
     //  Get.Create<LoginController>(() => LoginController());
 
     // TODO: implement build
-    return GetMaterialApp(
-      title: "Dependency Injection",
-      home: Scaffold(
-        appBar: AppBar(title: Text("Dependency Injection")),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              RaisedButton(
-                child: Text("Click Me"),
-                onPressed: () {
-                  // Instance will be created
-                  // Get.find<MyController>();
-                  //Find instance with tag name
-                  //Get.find<MyController>().incrementCounter();
-                },
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(title: Text("Dependency Injection")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              child: Text("Click Me"),
+              onPressed: () {
+                // Instance will be created with tag name
+                // Get.find<ControllerDependencyInjection>(tag: 'instance1');
+                // Instance will be created
+                // Get.find<ControllerDependencyInjection>();
+                //Find instance with tag name
+                Get.find<ControllerDependencyInjection>().incrementCounter();
+              },
+            ),
+          ],
         ),
       ),
     );
